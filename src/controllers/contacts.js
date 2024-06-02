d02b312b2c381c0063dd7dae1051d608f44d2b1c
IgorGra1 <gis1@ukr.net>
2024-06-02 15:10:08 +0300

    update

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -47,12 +47,10 @@ export const createContactController = async (req, res) => {
 
   const createdContact = await createContact(newContact);
 
-  const { __v, ...contactWithoutVersion } = createdContact.toObject();
-
   res.status(201).json({
     status: 201,
     message: 'Successfully created a contact!',
-    data: contactWithoutVersion,
+    data: createdContact,
   });
 };
 
