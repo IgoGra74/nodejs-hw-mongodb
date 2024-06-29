c4a477507224704e47ce516705dbd903b5762d9e
IgorGra1 <gis1@ukr.net>
2024-06-29 18:01:18 +0300

    update

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -24,6 +25,10 @@ export const setupServer = () => {
   );
 
   app.use(cors());
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use(
     pino({
